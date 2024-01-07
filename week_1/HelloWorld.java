055d11a3f85d40e2742c02ac4c338888f37676db
ARAVIND . S <78127293+aravind51097@users.noreply.github.com>
2024-01-07 21:53:47 +0530

    pushing first code

---
+++ week_1/HelloWorld.java
@@ -0,0 +1,16 @@
+package week_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Hello World!");
+		
+		StdOut.println("hello World!");
+	}
+
+}
